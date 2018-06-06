67132f8a955f0689505d08510f0e2b89b70a0274
TimaBag <tadilzhanov@gmail.com>
2018-06-07 00:06:36 +0600

    push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,8 +10,8 @@ class Footer extends Component{
 
   render(){
     return(
-      <div>
-        Footer
+      <div className="flex item-cnt justify-center">
+        <img src="https://d3n32ilufxuvd1.cloudfront.net/569f343f92f4e3e522d9ea93/925418/upload-09e52650-4d13-11e8-8b8d-6df9661c600b.png" url="https://d3n32ilufxuvd1.cloudfront.net/569f343f92f4e3e522d9ea93/925418/upload-09e52650-4d13-11e8-8b8d-6df9661c600b.png" style={{opacity: 1,width: 70}}/>
       </div>
     )
   }
